770ea3f6799dba4599461f90857ad84756bbd65c
shamirul <hudashamirul@gmail.com>
2024-09-28 21:20:24 +0530

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = Number(a);
+    if(a<0)
+    {
+        return "OOPS";
+    }
+    let f = [0,1,1];
+    for(let i=3;i<=a;i++)
+    {
+        let k = f[i-1]+f[i-2];
+        f.push(k);
+    }
+    return f[a];
 };
 
 // Do not edit below this line
