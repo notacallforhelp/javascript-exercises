938d8ba5c32b23c8ea07b485f2413c03669ea40d
shamirul <hudashamirul@gmail.com>
2024-09-29 01:14:14 +0530

    finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let res = people.map((person)=>{
+        if(!Object.hasOwn(person,'yearOfDeath'))
+        {
+            person['yearOfDeath']=new Date().getFullYear();
+        }
+        return person;
+    })
+    .sort((a,b)=>{
+        return (b.yearOfDeath-b.yearOfBirth)-(a.yearOfDeath-a.yearOfBirth);
+    });
+    return res[0];
 };
 
 // Do not edit below this line
